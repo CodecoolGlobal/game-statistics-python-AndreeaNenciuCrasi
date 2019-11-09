bf483363cf65bd8e1c4ac87910d32d51b80b7ae8
Andreea Nenciu Crasi <aanicaa@yahoo.com>
2019-11-09 13:47:31 +0200

    Export reports

--- export.py
+++ export.py
@@ -1,2 +1,19 @@
-
+import reports
 # Export functions
+
+result1 = reports.count_games("game_stat.txt")
+result2 = reports.decide("game_stat.txt", 2000)
+result3 = reports.get_latest("game_stat.txt")
+result4 = reports.count_by_genre("game_stat.txt", 'First-person shooter')
+result5 = reports.get_line_number_by_title("game_stat.txt", 'Half-Life')
+result6 = reports.sort_abc("game_stat.txt")
+result7 = reports.get_genres("game_stat.txt")
+result8 = reports.when_was_top_sold_fps("game_stat.txt")
+
+result_list = [result1, result2, result3,
+               result4, result5, result6, result7, result8]
+
+file = open('export_statistics_report.txt', "w")
+for result in result_list:
+    file.writelines(str(result))
+    file.write("\n")
--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,18 @@
-
+import reports
 # Export functions
+result1 = reports.get_most_played('game_stat.txt')
+result2 = reports.sum_sold('game_stat.txt')
+result3 = reports.get_selling_avg('game_stat.txt')
+result4 = reports.count_longest_title('game_stat.txt')
+result5 = reports.get_date_avg('game_stat.txt')
+result6 = reports.get_game('game_stat.txt', "Counter-Strike")
+result7 = reports.count_grouped_by_genre('game_stat.txt')
+result8 = reports.get_date_ordered('game_stat.txt')
+
+result_list = [result1, result2, result3,
+               result4, result5, result6, result7, result8]
+
+file = open('export_statistics_report2.txt', "w")
+for result in result_list:
+    file.writelines(str(result))
+    file.write("\n")
